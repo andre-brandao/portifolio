512d02f769e0abe6961e5c43b136270831720f89
andre-brandao <82166576+andre-brandao@users.noreply.github.com>
2024-11-21 20:36:22 -0300

    init

---
+++ astro.config.mjs
@@ -0,0 +1,23 @@
+// @ts-check
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import { defineConfig } from "astro/config";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://astro-i18n-starter.pages.dev",
+  integrations: [mdx(), sitemap()],
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en", "pt"],
+    routing: {
+      prefixDefaultLocale: true,
+      redirectToDefaultLocale: false,
+    },
+  },
+  markdown: {
+    shikiConfig: {
+      theme: "ayu-dark",
+    },
+  },
+});
---
+++ src/consts.ts
@@ -0,0 +1,18 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+import type { Multilingual } from "@/i18n";
+
+export const SITE_TITLE: string | Multilingual = "André Brandão Portfolio";
+
+export const SITE_DESCRIPTION: string | Multilingual = {
+  en: "André Brandão's Portfolio",
+  pt: "Portfólio André Brandão",
+};
+
+export const X_ACCOUNT: string | Multilingual = "@abc";
+
+export const NOT_TRANSLATED_CAUTION: string | Multilingual = {
+  en: "This page is not available in your language.",
+  pt: "Esta página não está disponível no seu idioma.",
+};
