c2ccdd704449814182ddc01a127e5a61870e5540
andre-brandao <82166576+andre-brandao@users.noreply.github.com>
2025-01-21 03:43:52 -0300

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ import svelte from "@astrojs/svelte";
 // https://astro.build/config
 export default defineConfig({
   site: "https://andrebrandao.dev",
-  integrations: [mdx(), sitemap(), tailwind(), svelte()],
+  integrations: [mdx(), sitemap(), tailwind(), svelte({ extensions: [".svelte"] })],
   i18n: {
     defaultLocale: "en",
     locales: ["en", "pt"],
