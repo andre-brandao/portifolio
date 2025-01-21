82c714a7c4c5c1605faed0cdf64517b7c98d0896
andre-brandao <82166576+andre-brandao@users.noreply.github.com>
2025-01-21 01:47:54 -0300

    add globe

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,12 @@ import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
+import svelte from "@astrojs/svelte";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://andrebrandao.dev",
-  integrations: [mdx(), sitemap(), tailwind()],
+  integrations: [mdx(), sitemap(), tailwind(), svelte()],
   i18n: {
     defaultLocale: "en",
     locales: ["en", "pt"],
