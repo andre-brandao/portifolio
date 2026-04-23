372d83d07efddaaf480a545bc04c11d2c4555488
andre-brandao <82166576+andre-brandao@users.noreply.github.com>
2026-04-22 22:00:21 -0300

    upgrade astro

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,17 +1,14 @@
 import { defineCollection, z } from 'astro:content';
+import { glob } from 'astro/loaders';
 
 const blog = defineCollection({
-  type: 'content',
+  loader: glob({ pattern: '**/*.{md,mdx}', base: './src/content/blog' }),
   schema: ({ image }) =>
     z.object({
       title: z.string(),
       description: z.string(),
       date: z.coerce.date(),
-      cover: image()
-        // .refine((img) => img.width >= 500, {
-        //   message: "cover must be at least 500px wide",
-        // })
-        .optional(),
+      cover: image().optional(),
       tags: z.array(z.string()),
     }),
 });
